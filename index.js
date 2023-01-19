6252ccad6badc369ce469c44a0d59f307e39f273
urunov <urunovfirdavsjon@gmail.com>
2023-01-19 22:18:21 +0500

    deploy test

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Hello, World!');
+const exporess = require('exporess');
+const app = express();
+
+const PORT = process.env.PORT || 8081;
+
+app.get('/', async (req, res) => {
+  try {
+    res.send('Hello, World');
+  } catch (e) {
+    res.status(404).send('Error: ' + e.message)
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
