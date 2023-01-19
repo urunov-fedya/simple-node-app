db9078d60cef8a5005adc96f436680c2c7bce1e5
urunov <urunovfirdavsjon@gmail.com>
2023-01-19 21:34:29 +0500

    first commit

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const http = require('http');
 
 const port = 9090
 
-http.createServer((res, req) => {
+http.createServer((_, req) => {
   req.end('hello');
 }).listen(port, () => console.log('listening on port ' + port));
