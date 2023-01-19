147a8bb975defded2697fc6c3a107aa5822c7663
urunov <urunovfirdavsjon@gmail.com>
2023-01-19 22:38:03 +0500

    removed lib

--- index.js
+++ index.js
@@ -1,24 +1,10 @@
 const express = require('express');
-const axios = require('axios');
 const app = express();
 const PORT = process.env.PORT || 8081;
 
 app.get('/', async (req, res) => {
-  const username = req.query.username || 'myogeshchavan97';
   try {
-    const result = await axios.get(
-      `https://api.github.com/users/${username}/repos`
-    );
-    const repos = result.data
-      .map((repo) => ({
-        name: repo.name,
-        url: repo.html_url,
-        description: repo.description,
-        stars: repo.stargazers_count
-      }))
-      .sort((a, b) => b.stars - a.stars);
-
-    res.send(repos);
+    res.send([1, 2, 3]);
   } catch (error) {
     res.status(400).send('Error while getting list of repositories');
   }
